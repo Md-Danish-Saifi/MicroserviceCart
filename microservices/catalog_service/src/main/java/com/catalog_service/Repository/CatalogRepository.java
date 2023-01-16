93af9d5e5c548692eb80f97a66ab1cf4001e00c8
Danish <mddanishsaifi2@gmail.com>
2023-01-16 14:30:35 +0530

    Revert "initial commit"
    This reverts commit 1690a96643e1252a4a9b1f0e8dbc0692415849c8.

--- microservices/catalog_service/src/main/java/com/catalog_service/Repository/CatalogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.catalog_service.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.catalog_service.Model.CatalogModel;
-
-public interface CatalogRepository extends JpaRepository<CatalogModel, Long>{
-
-}
--- microservices/user_service/src/main/java/com/user_service/Util/ApisEndPoint.java
+++
@@ -1,6 +0,0 @@
-package com.user_service.Util;
-
-public class ApisEndPoint {
-	public static final String LOGIN = "/login";
-	public static final String REGISTER = "/register";
-}
